89c771808d6135a0354e9c9e5a0f8e8697882463
igor <melnik.net@gmail.com>
2024-12-14 11:44:51 +0300

    The homework is done

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,7 @@ public class Main {
         System.out.println("Задача 3");
         int year = 2024;
         if (year > 1584) {
-            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
                 System.out.println(year + " год является високосным");
             } else {
                 System.out.println(year + " год не является високосным");
